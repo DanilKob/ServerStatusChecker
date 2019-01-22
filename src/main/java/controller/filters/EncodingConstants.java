8a7865ab0656782e9ece2ebc24d1334c89070e0f
Danylo_Kobzar <kobzardanils@gmail.com>
2019-01-22 23:06:35 +0200

    Create project structure

---
+++ src/main/java/controller/filters/EncodingConstants.java
@@ -0,0 +1,7 @@
+package controller.filters;
+
+public interface EncodingConstants {
+    String CONTEX_TYPE_TEXT_HTML = "text/html";
+    String REGUEST_ENCODING_UTF8 = "UTF-8";
+    String RESPONSE_ENCODING_UTF8 = "UTF-8";
+}
