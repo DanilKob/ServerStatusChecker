371e4b5603af3e555f42bf8e3d61f94b6bc85ade
Danylo_Kobzar <kobzardanils@gmail.com>
2019-01-25 03:08:52 +0200

    Update entities, add extra fields

--- src/main/java/model/checker/RevisionResult.java
+++ src/main/java/model/checker/RevisionResult.java
@@ -4,4 +4,12 @@ import java.util.List;
 
 public class RevisionResult {
     private List<ServerRevisionResult> serverRevisionResultList;
+
+    public List<ServerRevisionResult> getServerRevisionResultList() {
+        return serverRevisionResultList;
+    }
+
+    public void setServerRevisionResultList(List<ServerRevisionResult> serverRevisionResultList) {
+        this.serverRevisionResultList = serverRevisionResultList;
+    }
 }
